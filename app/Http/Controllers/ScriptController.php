2552b94473b410f345a98c364be23a218192f2e3
nicosli <nicosli@gmail.com>
2019-09-04 23:53:01 -0500

    se compone anio

--- app/Http/Controllers/ScriptController.php
+++ app/Http/Controllers/ScriptController.php
@@ -26,7 +26,7 @@ class ScriptController extends Controller
         for($mes=1; $mes<=7; $mes++){
             $mesChar = ($mes<10)? "0".$mes : $mes;
             $array[] = [
-                "fecha" => "2018-$mesChar-01",
+                "fecha" => "2019-$mesChar-01",
                 "hora" => "10:00:00",
                 "mes_informe" => $mes,
                 "anio_informe" => "2019"
